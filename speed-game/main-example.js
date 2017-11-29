00af6f2cb3e81fead57bbadaf31e96d5b05be844
Polina <pelageya913@gmail.com>
2017-11-29 15:21:27 +0200

    animate-car-2v

--- speed-game/main-example.js
+++ speed-game/main-example.js
@@ -15,17 +15,47 @@ var turn;
 var player;
 var currentSpeed = 0;
 var cursors;
+var moving;
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    land = game.add.tileSprite(0, 0, 800, 600, 'background');
-    land.fixedToCamera = true;
+    land = game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    //game.world.setBounds(0, 0, 1920, 1920);
+
+    // land.fixedToCamera = true;
+
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'user-car');
+    cursors = game.input.keyboard.createCursorKeys();
+    game.camera.follow(player);
+
+
+
+
+
+    game.world.setBounds(0, 0, 1920, 1200);
+
+    game.add.sprite(0, 0, 'background');
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.input.onDown.add(toggle, this);
+
+    function toggle() {
+
+        moving = (moving === 0) ? moving = 1 : moving = 0;
+
+    }
+
+
+
+
+
 
     road = game.add.group();
     road.enableBody = true;
 
-    track = road.create(0, game.world.height - 64, 'road');
+    track = road.create(0, game.world.height - 665, 'road');
 
     road.scale.setTo(0.5, 0.5);
 
@@ -44,54 +74,26 @@ function create() {
     turn = road.create(1450, 535, 'road');
 
 
-    player = game.add.sprite(10, game.world.height - 340, 'user-car');
+    player = game.add.sprite(10, game.world.height - 940, 'user-car');
     game.physics.arcade.enable(player);
 
 }
 
 function update() {
     game.physics.arcade.collide(player, turn);
-    cursors = game.input.keyboard.createCursorKeys();
-    player.body.velocity.x = 0;
+     cursors = game.input.keyboard.createCursorKeys();
+     player.body.velocity.x = 0;
 
 
 
-    if (cursors.left.isDown)
-    {
-        player.angle = 10;
-        currentSpeed = 100;
-    }
-    else if (cursors.right.isDown)
-    {
-        player.angle = -10;
-        currentSpeed = 100;
-    }
 
-    if (cursors.up.isDown)
-    {
-        currentSpeed = 300;
-    }
-    else
-    {
-        if (currentSpeed > 0)
-        {
-            currentSpeed += 2;
-        }
-    }
-
-    if (cursors.down.isDown)
-    {
-        currentSpeed = -100;
-    }
-    else
-    {
-        if (currentSpeed > 0)
-        {
-            currentSpeed -= 2;
-        }
-    }
 
     if (currentSpeed > 0)
+    {
+        game.physics.arcade.velocityFromRotation(player.angle, currentSpeed, player.body.velocity);
+    }
+
+    if (currentSpeed < 0)
     {
         game.physics.arcade.velocityFromRotation(player.angle, currentSpeed, player.body.velocity);
     }
@@ -99,6 +101,54 @@ function update() {
     land.tilePosition.x = -game.camera.x;
     land.tilePosition.y = -game.camera.y;
 
+    if (moving === 0)
+    {
+        if (cursors.up.isDown)
+        {
+            game.camera.y -= 4;
+        }
+        else if (cursors.down.isDown)
+        {
+            game.camera.y += 4;
+        }
+
+        if (cursors.left.isDown)
+        {
+            game.camera.x -= 4;
+        }
+        else if (cursors.right.isDown)
+        {
+            game.camera.x += 4;
+        }
+    }
+    else
+    {
+        if (cursors.left.isDown)
+        {
+            player.x -= 4;
+        }
+        else if (cursors.right.isDown)
+        {
+            player.x += 4;
+        }
+
+        if (cursors.up.isDown)
+        {
+            player.y -= 4;
+        }
+        else if (cursors.down.isDown)
+        {
+            player.y += 4;
+        }
+    }
+
 
 
 }
+
+// function render() {
+//
+//     game.debug.cameraInfo(game.camera, 32, 32);
+//     game.debug.spriteCoords(player, 32, 500);
+//
+// }
