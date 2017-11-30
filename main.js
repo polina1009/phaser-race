671cf1679ef94495e345fd90bc9a62787ef309db
Polina <pelageya913@gmail.com>
2017-11-30 14:13:22 +0200

    ex

--- main.js
+++ main.js
@@ -1,78 +1,67 @@
-// var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'SCENE', { preload: preload, create: create, update: update });
-//
-// var userCar;
-// var anotherCar;
-//
-// function preload() {
-//     game.load.image('road', 'assets/asphalt.jpg');
-//     game.load.image('playerCar', 'assets/car.png');
-//     game.load.image('computerCar', 'assets/comp-car.png');
-//     game.load.image('background', 'assets/green-grase.jpg');
-// }
-//
-// var playerCar;
-// // var computerCar;
-// // var road;
-//
-// // var computerCarSpeed = 190;
-// // var playerCarSpeed = 300;
-//
-//
-//
-// function createCar(x, y) {
-//     var car = game.add.sprite(x, y, 'playerCar');
-//     car.anchor.setTo(0.5, 0.5);
-//     // console.log(car);
-//
-//     // car.body.collideWorldBounds = true;
-//     // car.body.bounce.setTo(1, 1);
-//     // car.body.immovable = true;
-//
-//     return car;
-// }
-//
-//
-//
-// // var CarStarted = false;
-// //
-// // function startCar() {
-// //     if (!CarStarted) {
-// //         car.body.velocity.x = playerCarSpeed;
-// //         car.body.velocity.y = -playerCarSpeed;
-// //         CarStarted = true;
-// //     }
-// // }
-// //
-// function create() {
-//     game.add.tileSprite(0, 0, 600, 600, 'background');
-//     // var playerCar = game.add.sprite(200, 240, 'playerCar');
-//     // playerCar = createCar(game.world.centerX, 500);
-//
-//     userCar = game.add.group();
-//     anotherCar = game.add.group();
-//
-//     for (var i = 0; i < 4; i++) {
-//         anotherCar.create('computerCar');
-//     }
-//
-//     var playerCar = game.add.sprite(200, 240, 'playerCar');
-//
-//     userCar.add(playerCar);
-//
-//
-//
-//
-//     // computerCar = createCar(game.world.centerX, 200);
-//
-//     // road = game.add.sprite(game.world.centerX, game.world.centerY, 'road');
-//     // road.anchor.setTo(0.5, 0.5);
-//     // road.body.collideWorldBounds = true;
-//     // road.body.bounce.setTo(1, 1);
-//     //
-//     // game.input.onDown.add(startCar(), this);
-// }
-//
-// function update () {
-// }
-//
-//
+var game = new Phaser.Game(1280, 839, Phaser.AUTO, 'main_game', { preload: preload, create: create, update: update });
+
+function preload() {
+    game.load.spritesheet('map','assets/map.jpg');
+    game.load.spritesheet('car','assets/car.png');
+}
+
+var cursors;
+var velocity = 0;
+function create() {
+    /*Enable Phyics Engine*/
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    /*Adding Map*/
+    var map = game.add.sprite(0,0,'map');
+    /*Adding car*/
+    car = game.add.sprite(570,100,'car');
+    game.physics.p2.enable(car);
+    car.body.angle = 90;
+
+    cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update()
+{
+    /*Update Velocity*/
+    if (cursors.up.isDown && velocity <= 400) {
+        velocity+=7;
+    }
+    else {
+        if (velocity >= 7)
+            velocity -= 7;
+    }
+
+    /*Set X and Y Speed of Velocity*/
+    car.body.velocity.x = velocity * Math.cos((car.angle-90)*0.01745);
+    car.body.velocity.y = velocity * Math.sin((car.angle-90)*0.01745);
+
+    /*Rotation of Car*/
+    if (cursors.left.isDown)
+        car.body.angularVelocity = -5*(velocity/1000);
+    else if (cursors.right.isDown)
+        car.body.angularVelocity = 5*(velocity/1000);
+    else
+        car.body.angularVelocity = 0;
+}
+
+
+game.load.physics("collision","assets/collision.json");
+
+ //реалтзация столкновение
+var carCollisionGroup = game.physics.p2.createCollisionGroup();
+var buildingCollisionGroup = game.physics.p2.createCollisionGroup();
+game.physics.p2.updateBoundsCollisionGroup();
+var carCollisionGroup = game.physics.p2.createCollisionGroup();
+var buildingCollisionGroup = game.physics.p2.createCollisionGroup();
+game.physics.p2.updateBoundsCollisionGroup();
+var building = game.add.sprite(640,420,'building');
+game.physics.p2.enable(building);
+building.body.kinematic = true;
+building.body.clearShapes();
+building.body.loadPolygon('collision','building');
+car.body.setCollisionGroup(carCollisionGroup);
+building.body.setCollisionGroup(buildingCollisionGroup);
+car.body.collides([carCollisionGroup,buildingCollisionGroup]);
+building.body.collides([buildingCollisionGroup,carCollisionGroup]);
+game.physics.p2.enable(building,true);
+
